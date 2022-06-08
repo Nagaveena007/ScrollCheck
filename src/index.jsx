98722c035eb72f2dca92276ec90e8c7c2b7ffe47
Nagaveena Hallikeri <72273357+Nagaveena007@users.noreply.github.com>
2022-06-08 17:19:59 +0200

    .

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,14 @@
-import React, { useLayoutEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom'
 
-import ScrollCheck from './pages/ScrollCheck'
-import MyPage from './pages/MyPage'
-const ScrollToTop = withRouter(({ children, location: { pathname } }) => {
-  useLayoutEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
-  return children || null
-})
+import MyPage from './components/MyPage'
+
 
 function App() {
   return (
     <Router>
         <div className="App" />
-        <Route exact path="/page" component={ScrollCheck} />
         <Route exact path="/" component={MyPage} />
     </Router>
   )
